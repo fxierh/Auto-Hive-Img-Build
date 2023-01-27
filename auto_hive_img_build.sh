9aa6479ac50126e6714a8e699082ce1d567d1828
Feilian Xie <fxie@redhat.com>
2023-01-27 15:02:12 +0800

    Use local commit id after pulling instead of querying remote directly with git ls-remote

--- auto_hive_img_build.sh
+++ auto_hive_img_build.sh
@@ -10,22 +10,22 @@ set -eux
 cd /home/cloud-user/hive
 
 # Hash of the latest master commit of the remote repo
-remote_hash=$(git ls-remote -q | grep master | awk '{print $1}' | cut -c 1-7)
-# local_hash=$(git log -n 1 master | head -n 1 | awk '{print $2}' | cut -c 1-7)
+git pull
+local_hash=$(git log -n 1 master | head -n 1 | awk '{print $2}' | cut -c 1-7)
+# remote_hash=$(git ls-remote -q | grep master | awk '{print $1}' | cut -c 1-7)
 
 # Hash of the latest image tag on quay.io (using quay's api)
 # shellcheck disable=SC2154
 quay_latest=$(curl -s -X GET https://quay.io/api/v1/repository/"$quay_username"/"$quay_repo"/tag/ | jq -rs '.[].tags | map(select(.expiration | not)) | sort_by(.start_ts) | reverse | .[0].name')
 
 # shellcheck disable=SC2086
-if [ "${remote_hash}" != ${quay_latest} ]; then
-    echo "Latest master commit of remote ($remote_hash) != latest image tag on quay.io ($quay_latest), pulling:"
-    git pull
+if [ "${local_hash}" != ${quay_latest} ]; then
+    echo "Latest master commit ($local_hash) != latest image tag on quay.io ($quay_latest),
+        building & pushing new Hive image:"
 
-    echo "Building & pushing new Hive image:"
     # shellcheck disable=SC2154
-    podman login quay.io --username ${quay_username} --password ${quay_password}
-    IMG=quay.io/fxierh/hive:${remote_hash} make buildah-dev-push
+    podman login quay.io --username "${quay_username}" --password "${quay_password}"
+    IMG=quay.io/fxierh/hive:"${local_hash}" make buildah-dev-push
 else
-    echo "Latest master commit of remote ($remote_hash) == latest image tag on quay.io ($quay_latest), doing nothing. "
+    echo "Latest master commit ($local_hash) == latest image tag on quay.io ($quay_latest), doing nothing. "
 fi
